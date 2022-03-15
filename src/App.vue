77e4227608f42f8df9a2b3cfedb813515534ae13
Liugenhao <lgh.gg@qq.com>
2022-03-15 17:11:46 +0800

    file

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <p> {{ child }}</p>
+  <div style="margin-bottom:20px;">
+    <button @click="pClick">click</button>
+  </div>
+ 
+ 
+  <Child @childClick="childrenClick" :show="isShow"/>
+
+
+ 
+</template>
+
+<script setup>
+
+import HelloWorld from './components/HelloWorld.vue'
+import Child from './components/child.vue'
+
+import { ref } from 'vue'
+
+const child = ref('')
+
+const isShow = ref(false)
+
+const emit = defineEmits(['childClick'])
+
+const childrenClick = (data) => {
+  emit('childClick',data)
+  child.value = data
+}
+
+const pClick = () =>{
+  !isShow.value ? isShow.value = true : isShow.value = false
+}
+
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/child.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="child">
+    <button @click="childrenClick('来自子组件传来的值')">子组件 Click</button>
+
+    <div>来自父组件传来的值： {{ show }}</div>
+
+  </div>
+</template>
+<script setup>
+
+  import { ref ,watch } from 'vue'
+  
+  const test = ref("子组件内容")
+
+  const emit = defineEmits(['childClick'])
+
+  const childrenClick = (data) =>{
+    emit('childClick', data)
+  }
+
+const props = defineProps({
+  show:{
+    type:Boolean,
+    defulat:false
+  }
+})
+
+watch (
+  () => {
+    props.show
+  }
+)
+
+
+</script>
+<style scoped>
+
+</style>
