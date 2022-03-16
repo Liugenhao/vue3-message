b90e5ae6896f1e6f438eb361a7bc4fa7bcf71796
Liugenhao <lgh.gg@qq.com>
2022-03-16 12:04:02 +0800

    1

--- src/components/child.vue
+++ src/components/child.vue
@@ -24,7 +24,9 @@ const props = defineProps({
     defulat:false
   }
 })
-
+// watch 函数内部调用了 doWatch 函数
+// 调用前会在非生产环境下判断第二个参数 cb 是不是一个函数
+// 如果不是则会报警告以告诉用户应该使用 watchEffect(fn, options) API
 watch (
   () => {
     props.show
